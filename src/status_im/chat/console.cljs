023175def9b47547b099ef9ffca958ae111fa912
janherich <jan.herich@gmail.com>
2018-02-24 01:18:01 +0100

    Works on commands/requests

--- src/status_im/chat/console.cljs
+++ src/status_im/chat/console.cljs
@@ -27,8 +27,8 @@
 
 (def move-to-internal-failure-message
   (console-message {:message-id   chat-const/move-to-internal-failure-message-id
-                    :content      {:command "grant-permissions"
-                                   :content (i18n/label :t/move-to-internal-failure-message)}
+                    :content      {:request-command "grant-permissions"
+                                   :content         (i18n/label :t/move-to-internal-failure-message)}
                     :content-type const/content-type-command-request}))
 
 (defn passphrase-messages [mnemonic signing-phrase crazy-math-message?]
@@ -57,8 +57,8 @@
 
 (def intro-message1
   (console-message {:message-id   chat-const/intro-message1-id
-                    :content      {:command "password"
-                                   :content (i18n/label :t/intro-message1)}
+                    :content      {:request-command "password"
+                                   :content         (i18n/label :t/intro-message1)}
                     :content-type const/content-type-command-request}))
 
 (def chat
